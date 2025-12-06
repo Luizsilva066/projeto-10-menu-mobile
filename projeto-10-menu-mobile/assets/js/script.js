d726dead5423a6c7c81eabf5402a7d9034d499e9
Luiz Henrique <luizsales663cl@gmail.com>
2025-12-05 23:24:52 -0300

    Add files via upload

---
+++ projeto-10-menu-mobile/assets/js/script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const mainNav = document.querySelector('.main-nav');
+
+menuToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('is-open');
+    menuToggle.classList.toggle('is-open');
+});
